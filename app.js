51cfe71d737e499f5ba5dbb52b1af42a3299f668
Nisarg6502 <nisarg6502@gmail.com>
2022-12-03 21:38:04 +0530

    package.json and dependencies understood

--- app.js
+++ app.js
@@ -31,8 +31,40 @@
     add a gitignore file and add the command '/<nodeModuleFolderName>'
 */
 
+/*
+    Since we haven't pushed the node modules folder to the repository
+    those who try to clone it can get the modules required for the 
+    project by running the command 'npm install' which will check for 
+    dependencies in the package.json folder and install the dependencies
+    mentioned in it
+*/
+
+//  Nodemon is a popular tool that is used for the development of applications based on node.js. 
+//  It simply restarts the node application whenever it observes the changes in the file present 
+//  in the working directory of your project.
+
+/*
+    devDependencies are those packages in the package. json file that you 
+    need only for project development purposes. Example- Babel, Webpack, etc. 
+    You require these packages to test and run your project on the localhost.
+    We don't share these dependencies for production and to the users.
+*/
+
+/*
+    "scripts": {
+        "start":"node app.js",
+        "dev":"nodemon app.js"
+    },
+*/
+
+/*
+    package-lock.json is necessary because sometimes a particular dependency 
+    is dependent on another dependency. We need to make sure the versions of these
+    packages remain the same for the user as well otherwise we may face some bugs.
+*/
+
 const _ = require('lodash')
 
 const items = [1,[2,[3,[4]]]]
 const newItems = _.flattenDeep(items)
-console.log(newItems) //[ 1, 2, 3, 4 ]
+console.log(newItems + " hi") //[ 1, 2, 3, 4 ]
